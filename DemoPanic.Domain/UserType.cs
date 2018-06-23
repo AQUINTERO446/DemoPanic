f30d021d64d0262b8fffbe4fc6336266f9373224
Andres Quintero Galvan <aquintero446@gmail.com>
2018-06-23 11:35:45 -0500

    Actualizacion y fix: runtime, autostart Mono

---
+++ DemoPanic.Domain/UserType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoPanic.Domain
+{
+    public class UserType
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0}es requerido.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} solo puede tener un maximo de {1} caracteres.")]
+        [Index("UserType_Name_Index", IsUnique = true)]
+        [DataType(DataType.EmailAddress)]
+        public string  Name { get; set; }
+    }
+}
