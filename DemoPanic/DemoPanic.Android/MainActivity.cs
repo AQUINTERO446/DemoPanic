a21eedb283e5c039985c49fcdb6cf2c87e1f6467
Andres Quintero Galvan <aquintero446@gmail.com>
2018-06-26 10:55:10 -0500

    FixCamera

--- DemoPanic/DemoPanic.Android/MainActivity.cs
+++ DemoPanic/DemoPanic.Android/MainActivity.cs
@@ -6,10 +6,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace DemoPanic.Droid
 {
-    [Activity(Label = "DemoPanic", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "DemoPanic",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -23,6 +29,18 @@ namespace DemoPanic.Droid
             Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
+
+        public override void OnRequestPermissionsResult(
+            int requestCode, 
+            string[] permissions,
+            [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(
+                requestCode, 
+                permissions, 
+                grantResults);
+        }
+
+        }
     }
-}
 
