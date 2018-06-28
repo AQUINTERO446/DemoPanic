69860801f21e2217e059727815a1dfe25b86ad5f
Andres Quintero Galvan <aquintero446@gmail.com>
2018-06-28 10:10:03 -0500

    xxx

--- DemoPanic/DemoPanic/Views/UbicationsPage.xaml.cs
+++ DemoPanic/DemoPanic/Views/UbicationsPage.xaml.cs
@@ -5,38 +5,33 @@
     using Xamarin.Forms.Xaml;
     using ViewModels;
     using System;
+    using System.Globalization;
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UbicationsPage : ContentPage
 	{
 
-        #region Services
-        GeolocatorService geolocatorService;
-        #endregion
-
         #region Constructors
         public UbicationsPage()
         {
             InitializeComponent();
-            geolocatorService = new GeolocatorService();
             MoveMapToCurrentPosition();
         }
         #endregion
 
         #region Methods
-        async void MoveMapToCurrentPosition()
+        void MoveMapToCurrentPosition()
         {
-            await geolocatorService.GetLocation();
-            if (geolocatorService.Latitude != 0 ||
-                geolocatorService.Longitude != 0)
-            {
-                var position = new Position(
-                    geolocatorService.Latitude,
-                    geolocatorService.Longitude);
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ",";
+            var user = MainViewModel.GetInstance().User;
+            var position = new Position(
+                    Convert.ToDouble(user.Latitude, provider),
+                    Convert.ToDouble(user.Longitude, provider));
                 MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
                     position,
                     Distance.FromKilometers(6.5)));
-            }
+            
             LoadPins();
         }
         /// <summary>
@@ -52,26 +47,7 @@
                 MyMap.Pins.Add(pin);
             }
         }
-        /*
-        private void RefreshLocation_Clicked(object sender, EventArgs e)
-        {
-            RefreshLocation();
-        }
-
-        public async void RefreshLocation()
-        {
-            await geolocatorService.GetLocation();
-            if (geolocatorService.Latitude != 0 ||
-                geolocatorService.Longitude != 0)
-            {
-                UserLocationLabel.Text = String.Format(
-                    "Lat. : {0:0.000000} / Long. : {1:0.000000}",
-                    geolocatorService.Latitude,
-                    geolocatorService.Longitude);
-
-            }
-        }
-        */
+        
         #endregion
 
     }
